28eb5c4b153911d83a4bc3dfa6b12706cc4cebbf
meloncatty <k.handel66@gmail.com>
2018-04-22 13:06:33 -0600

    begin app.js add test for app.js, clean syntax in files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should have default state', () => {
+    const renderedApp = shallow(<App />)
+    const toDo = ''
+    const toDoList = []
+    expect(renderedApp.state('toDo')).toEqual(toDo)
+    expect(renderedApp.state('toDoList')).toEqual(toDoList)
+  })
+})
